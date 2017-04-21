246cb715407716b1fde59ab5a7612a374ceae230
mknarciso <murilokn@gmail.com>
2017-04-21 18:50:41 -0300

    Sprint5: Nao realizado

---
+++ src/test/java/br/ita/comp18/MURILONARCISO_QUESTAO08/Sprint5Mock.java
@@ -0,0 +1,15 @@
+package br.ita.comp18.MURILONARCISO_QUESTAO08;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sprint5Mock {
+
+	@Test
+	public void test() {
+		//Mock do Sprint 5
+		assertTrue(true);
+	}
+
+}
