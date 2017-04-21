48e02aa59ac46728da16727c3349eb24a16f8395
mknarciso <murilokn@gmail.com>
2017-04-21 16:58:27 -0300

    small fix

--- src/test/java/br/ita/comp18/MURILONARCISO_QUESTAO08/Sprint1DB.java
+++ src/test/java/br/ita/comp18/MURILONARCISO_QUESTAO08/Sprint1DB.java
@@ -12,13 +12,9 @@ import java.util.Date;
 public class Sprint1DB extends Admin
 {
 	private static UserServer userDB = new UserServer();
-    //private static UserDB userDB = mock(UserDB.class);
 	private static BlockServer blockDB = new BlockServer();
-    //private static BlockDB blockDB = mock(BlockDB.class);
 	private static BookServer bookDB = new BookServer();
-    //private static BookDB bookDB = mock(BookDB.class);
 	private static LoanServer loanDB = new LoanServer();
-    //private static LoanDB loanDB = mock(LoanDB.class);
     
 	public Sprint1DB(){
 		super(userDB,blockDB,bookDB,loanDB);
@@ -28,33 +24,28 @@ public class Sprint1DB extends Admin
 	public void CreateUser()  {
 		createUser("joao");
 		assertTrue(exists("joao"));
-		//verify(userDB, times(1)).newUser("joao");
 	}
 	@Test
 	public void RemoveUser()  {
 		createUser("joao");
 		removeUser("joao");
 		assertFalse(exists("joao"));
-		//verify(userDB, times(1)).removeUser("joao");
 	}
 	@Test
 	public void BlockUser()  {
 		Date today = new Date(2017,04,17);
 		blockUser("joao", 3, today);
 		assertTrue(blockDB.isBlocked("joao"));
-		//verify(blockDB, times(1)).newBlock("joao",today,3);
 		
 	}
 	@Test
 	public void NotBlockedTryToLend() {
-		//when(blockDB.isBlocked("joao")).thenReturn(false);
 		blockDB.removeBlocks("joao");
 		assertTrue(lendBook(357, "joao"));
 	}
 	@Test
 	public void BlockedTryToLend() {
 		Date today = new Date(2017,04,17);
-		//when(blockDB.isBlocked("joao")).thenReturn(true);
 		blockUser("joao", 3, today);
 		assertFalse(lendBook(357, "joao"));
 		
--- src/test/java/br/ita/comp18/MURILONARCISO_QUESTAO08/Sprint3DB.java
+++ src/test/java/br/ita/comp18/MURILONARCISO_QUESTAO08/Sprint3DB.java
@@ -1,47 +1,35 @@
 package br.ita.comp18.MURILONARCISO_QUESTAO08;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
 public class Sprint3DB extends User
 {
 	private static UserServer userDB = new UserServer();
-    //private static UserDB userDB = mock(UserDB.class);
 	private static BookServer bookDB = new BookServer();
-    //private static BookDB bookDB = mock(BookDB.class);
 	private static LoanServer loanDB = new LoanServer();
-    //private static LoanDB loanDB = mock(LoanDB.class);
     
 	public Sprint3DB(){
 		super(userDB,bookDB,loanDB);
 	}
 	@Test
 	public void UserAccess() {
-		//when(userDB.loginUser("joao")).thenReturn(true);
 		assertTrue(login("joao"));
 		
 	}
 	@Test
 	public void SearchBookNotAuthenticated() {
-		//when(bookDB.getIdByTitle("Star Wars")).thenReturn(357);
 		assertEquals(0,findTitle("Star Wars"));
 	}
 	@Test
 	public void SearchBookAuthenticated() {
-		//when(userDB.loginUser("joao")).thenReturn(true);
-		//when(bookDB.getIdByTitle("Star Wars")).thenReturn(357);
 		login("joao");
 		assertEquals(1,findTitle("Star Wars"));
 		logout();
 	}
 	@Test
 	public void GetBookStatusDisponível() {
-		//when(userDB.loginUser("joao")).thenReturn(true);
-		//when(bookDB.getIdByTitle("Star Wars")).thenReturn(357);
-		//when(bookDB.getStatus(357)).thenReturn(2);
 		login("joao");
 		assertEquals(1,findTitle("Star Wars"));
 		int bookId = findTitle("Star Wars");
@@ -52,9 +40,6 @@ public class Sprint3DB extends User
 	}
 	@Test
 	public void GetBookStatusEmprestado() {
-		//when(userDB.loginUser("joao")).thenReturn(true);
-		//when(bookDB.getIdByTitle("Star Wars")).thenReturn(357);
-		//when(bookDB.getStatus(357)).thenReturn(2);
 		login("joao");
 		assertEquals(1,findTitle("Star Wars"));
 		int bookId = findTitle("Star Wars");
